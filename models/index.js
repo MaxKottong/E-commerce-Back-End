dabfee7da8e960cb317cf2b84521c6b34525f49b
Max Kottong <max.kottong@gmail.com>
2022-08-28 15:07:38 -0400

    Updated index for models

--- models/index.js
+++ models/index.js
@@ -6,22 +6,34 @@ const ProductTag = require('./ProductTag');
 
 // Products belongsTo Category
 Product.belongsTo(Category, {
-
+    foreignKey: 'category_id'
 });
 
 // Categories have many Products
 Category.hasMany(Product, {
-
+    foreignKey: 'category_id'
 });
+
+ProductTag.belongsTo(Product, {
+    foreignKey: 'product_id'
+})
+
+ProductTag.belongsTo(Tag, {
+    foreignKey: 'tag_id'
+})
 
 // Products belongToMany Tags (through ProductTag)
 Product.belongsToMany(Tag, {
-    through: ProductTag
+    through: ProductTag,
+    as: 'product_tags',
+    foreignKey: 'product_id'
 });
 
 // Tags belongToMany Products (through ProductTag)
 Tag.belongsToMany(Product, {
-    through: ProductTag
+    through: ProductTag,
+    as: 'product_tags',
+    foreignKey: 'tag_id'
 });
 
 module.exports = {
