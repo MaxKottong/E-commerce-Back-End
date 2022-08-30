d191bd0e4104f4e1f179f5ff77b20440fea160b6
Max Kottong <max.kottong@gmail.com>
2022-08-30 13:56:55 -0400

    Added tag routes

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -41,7 +41,7 @@ router.post('/', (req, res) => {
     Category.create({
         category_name: req.body.category_name
     })
-        .then(data => res.json(createData))
+        .then(data => res.json(data))
         .catch(err => {
             console.log(err);
             res.status(500).json(err);
